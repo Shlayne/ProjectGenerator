490339aec4514a58032d5e7c8389db1b0b6bbfbf
Bruce Cosgrove <1901.brc@gmail.com>
2022-09-22 22:29:19 -0700

    Finished this, for now.

--- ProjectGenerator/src/main.cpp
+++ ProjectGenerator/src/main.cpp
@@ -1,166 +1,266 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <filesystem>
+#include <functional>
+#include <sstream>
 
 #if SYSTEM_WINDOWS
 	#define EMPTY_FILE "nul"
+	#define START_PROCESS_BEGIN "start "
+	#define START_PROCESS_END
 #else
 	#define EMPTY_FILE "/dev/null"
+	#define START_PROCESS_BEGIN
+	#define START_PROCESS_END " &"
 #endif
+
+enum class ReturnCode : int
+{
+	Success = 0,
+	ShowHelpMessage,
+	GitHubCLIMissing,
+	FileAlreadyExists,
+	CouldntCreateDirectory,
+	CouldntCreateRepository,
+	CouldntCloneRepository,
+	CouldntReadFile,
+	CouldntWriteFile,
+	CouldntGenerateProjects,
+	CouldntOpenVSSolution,
+};
+
+ReturnCode Run(int argc, const char* const* argv);
 
 int main(int argc, char** argv)
 {
-	/* TODO: 
-		1) Finish adding error messages for when things go wrong.
-		2) Streamline the system calls.
-		3) Make something like this possible:
-			"ProjectGenerator.exe Calculator C:\Workspace\Programming\Dev\C++ --olc --public"
-			where "--public" can be "--private" or ommitted,
-			and "--olc" can be ommitted which changes the repo it uses as a template.
-		4) Figure out how to deal with (aka remove) the pause at the end of calling "GenerateProjects.bat".
-		5) Potentially add a file to cache the directory so I don't have to type it each time.
-	*/
-
-	bool showHelpMessage = false;
-	if (argc == 1 || argc == 2 && std::string_view("--help") == argv[1])
-		showHelpMessage = true;
-	else if (argc > 1)
+	ReturnCode rc = Run(argc, argv);
+	int irc = static_cast<int>(rc);
+	if (rc != ReturnCode::Success)
 	{
-		std::filesystem::path projectDirectory = argv[1];
-		if (argc > 2)
-			projectDirectory = argv[2] / projectDirectory;
-
-		std::error_code error;
-		if (std::filesystem::create_directory(projectDirectory, error))
+		if (rc == ReturnCode::ShowHelpMessage)
 		{
-			if (!system("gh > " EMPTY_FILE " 2>&1"))
+			rc = ReturnCode::Success;
+			std::cout << "Usage: " << argv[0] << " [<ProjectName> [options] | --help]\n\n";
+			std::cout << "Options:\n";
+			std::cout << "   --dir filepath   Set the directory of the project\n";
+			std::cout << "   --private        Make the project private\n";
+			std::cout << "   --olc            Use OLCTemplate instead of ProjectTemplate\n";
+			std::cout << '\n';
+		}
+		else
+		{
+			constexpr const char* errorMessages[]
 			{
-				std::string command = "gh repo create ";
-				command.append(1, '"').append(argv[1]).append(1, '"').append(" --public --template Shlayne/ProjectTemplate");
-
-				if (!system(command.c_str()))
-				{
-					command = "gh repo clone \"";
-					command.append(argv[1]).append(1, '"');
-					if (argc > 2)
-						command.append(" \"").append(argv[2]).append(1, '"');
-
-					if (!system(command.c_str()))
-					{
-						constexpr char prjName[] = "__PROJECT_NAME__";
-						constexpr auto prjNameLength = sizeof(prjName) / sizeof(*prjName) - 1;
-						constexpr char wksName[] = "__WORKSPACE_NAME__";
-						constexpr auto wksNameLength = sizeof(wksName) / sizeof(*wksName) - 1;
-
-						// Rename "<dir>/<ProjectName>/__PROJECT_NAME__" to "<dir>/<ProjectName>/<ProjectName>".
-						std::filesystem::path oldPath = projectDirectory;
-						oldPath /= prjName;
-						std::filesystem::path newPath = projectDirectory;
-						newPath /= argv[1];
-						std::filesystem::rename(oldPath, newPath, error);
-
-						// Replace the first "__PROJECT_NAME__" in "<dir>/<ProjectName>/<ProjectName>/premake5.lua" with "<ProjectName>".
-						{
-							std::filesystem::path filepath = newPath / "premake5.lua";
-							std::ifstream inFile(filepath);
-							if (inFile.is_open())
-							{
-								std::stringstream inFileStream;
-								inFileStream << inFile.rdbuf();
-								inFile.close();
-
-								std::string file = inFileStream.str();
-								file.replace(file.find(prjName), prjNameLength, argv[1]);
-
-								std::ofstream outFile(filepath);
-								if (outFile.is_open())
-								{
-									outFile.write(file.c_str(), file.size());
-									outFile.close();
-								}
-								else
-								{
-									// todo
-								}
-							}
-							else
-							{
-								// todo
-							}
-						}
-
-						// Replace the first and last "__PROJECT_NAME__" in "<dir>/<ProjectName>/premake5.lua" with "<ProjectName>"
-						// and "__WORKSPACE_NAME__" with "<ProjectName>".
-						{
-							std::filesystem::path filepath = projectDirectory / "premake5.lua";
-							std::ifstream inFile(filepath);
-							if (inFile.is_open())
-							{
-								std::stringstream inFileStream;
-								inFileStream << inFile.rdbuf();
-								inFile.close();
-
-								std::string file = inFileStream.str();
-								file.replace(file.find(wksName), wksNameLength, argv[1]);
-								file.replace(file.find(prjName), prjNameLength, argv[1]);
-								file.replace(file.rfind(prjName), prjNameLength, argv[1]);
-
-								std::ofstream outFile(filepath);
-								if (outFile.is_open())
-								{
-									outFile.write(file.c_str(), file.size());
-									outFile.close();
-								}
-								else
-								{
-									// todo
-								}
-							}
-							else
-							{
-								// todo
-							}
-						}
-
-						// Call "GenerateProjects.bat".
-						command = "pushd \"";
-						command.append(projectDirectory.string())
-							.append(1, std::filesystem::path::preferred_separator)
-							.append("Scripts\" && call \".")
-							.append(1, std::filesystem::path::preferred_separator)
-							.append("GenerateProjects.bat\" && popd");
-						if (!system(command.c_str()))
-						{
-							command = argv[1];
-							command.append(1, std::filesystem::path::preferred_separator)
-								.append(argv[1])
-								.append(".sln");
-							if (!system(command.c_str()))
-							{
-
-							}
-							else
-								std::cerr << "Failed to open visual studio solution.\n";
-						}
-						else
-							std::cerr << "Failed to generate projects.\n";
-					}
-					else
-						std::cerr << "Failed to clone repository.\n";
-				}
-				else
-					std::cerr << "Failed to create repository.\n";
-			}
-			else
-				std::cerr << "Must have GitHub CLI installed. Get it here: \"https://cli.github.com/\".\n";
+				"Must have GitHub CLI installed. Get it here: https://cli.github.com/",
+				"A file already exists at the project directory.",
+				"Couldn't create project directory.",
+				"Couldn't create repository.",
+				"Couldn't clone repository.",
+				"Couldn't read file.",
+				"Couldn't write file.",
+				"Couldn't generate projects.",
+				"Couldn't open Visual Studio solution.",
+			};
+			std::cerr << errorMessages[irc - 2] << '\n';
 		}
 	}
+	return irc;
+}
+
+static constexpr char olcName[] = "OLCTemplate";
+static constexpr auto olcNameLength = sizeof(olcName) / sizeof(*olcName) - 1;
+static constexpr char prjName[] = "__PROJECT_NAME__";
+static constexpr auto prjNameLength = sizeof(prjName) / sizeof(*prjName) - 1;
+static constexpr char wksName[] = "__WORKSPACE_NAME__";
+static constexpr auto wksNameLength = sizeof(wksName) / sizeof(*wksName) - 1;
+
+ReturnCode EditFile(const std::filesystem::path& filepath, const std::function<void(std::string&)>& func);
+void ReplaceOLCTemplateWithProjectName(std::string& file, const char* arg1);
+ReturnCode ReplaceOLCTemplateWithProjectNameInFiles(const std::vector<std::filesystem::path>& filepaths, const char* arg1);
+
+ReturnCode Run(int argc, const char* const* argv)
+{
+	if (argc == 1 || (argc > 1 && std::string_view("--help") == argv[1]))
+		return ReturnCode::ShowHelpMessage;
+	else if (argc > 1)
+	{
+		if (system("gh > " EMPTY_FILE " 2>&1"))
+			return ReturnCode::GitHubCLIMissing;
+
+		std::filesystem::path directory;
+		bool isPrivate = false;
+		bool useOLCTemplate = false;
+
+		for (int argi = 2; argi < argc; )
+		{
+			std::string_view arg = argv[argi++];
+			if (arg == "--dir")
+				directory = argv[argi++];
+			else if (arg == "--private")
+				isPrivate = true;
+			else if (arg == "--olc")
+				useOLCTemplate = true;
+			else
+				return ReturnCode::ShowHelpMessage;
+		}
+
+		std::filesystem::path projectDirectory = directory / argv[1];
+
+		std::error_code error;
+		if (std::filesystem::exists(projectDirectory, error) || error)
+			return ReturnCode::FileAlreadyExists;
+
+		if (!std::filesystem::create_directory(projectDirectory, error) || error)
+			return ReturnCode::CouldntCreateDirectory;
+
+		// Create the repository.
+		std::string command = "gh repo create \"";
+		command.append(argv[1]).append("\" --");
+		command.append(isPrivate ? "private" : "public");
+		command.append(" --template Shlayne/");
+		command.append(useOLCTemplate ? "OLCTemplate" : "ProjectTemplate");
+		if (system(command.c_str()))
+			return ReturnCode::CouldntCreateRepository;
+
+		// Clone the repository.
+		command = "gh repo clone \"";
+		command.append(argv[1]).append(1, '"');
+		if (!directory.empty())
+			command.append(" \"").append(projectDirectory.string()).append(1, '"');
+		if (system(command.c_str()))
+			return ReturnCode::CouldntCloneRepository;
+
+		std::filesystem::path projectProjectDirectory = projectDirectory;
+		projectProjectDirectory /= argv[1];
+
+		// Rename the vs solution's project folder to "<dir>/<ProjectName>/<ProjectName>".
+		std::filesystem::rename(projectDirectory / (useOLCTemplate ? olcName : prjName), projectProjectDirectory, error);
+
+		// Replace the first "__PROJECT_NAME__" in "<dir>/<ProjectName>/<ProjectName>/premake5.lua" with "<ProjectName>".
+		ReturnCode rc = EditFile(projectProjectDirectory / "premake5.lua",
+		[argv, useOLCTemplate](std::string& file)
+		{
+			const char* name = prjName;
+			size_t nameLength = prjNameLength;
+			if (useOLCTemplate)
+			{
+				name = olcName;
+				nameLength = olcNameLength;
+			}
+			file.replace(file.find(name), nameLength, argv[1]);
+		});
+		if (rc != ReturnCode::Success)
+			return rc;
+
+		rc = EditFile(projectDirectory / "premake5.lua",
+		[argv, useOLCTemplate](std::string& file)
+		{
+			if (useOLCTemplate)
+				ReplaceOLCTemplateWithProjectName(file, argv[1]);
+			else
+			{
+				// Replace the first and last "__PROJECT_NAME__" in "<dir>/<ProjectName>/premake5.lua" with "<ProjectName>"
+				// and "__WORKSPACE_NAME__" with "<ProjectName>".
+				file.replace(file.find(wksName), wksNameLength, argv[1]);
+				file.replace(file.find(prjName), prjNameLength, argv[1]);
+				file.replace(file.rfind(prjName), prjNameLength, argv[1]);
+			}
+		});
+		if (rc != ReturnCode::Success)
+			return rc;
+
+		if (useOLCTemplate)
+		{
+			std::filesystem::path projectProjectSrcDirectory = projectProjectDirectory / "src";
+			std::filesystem::path projectProjectSrcOLCTemplateH = projectProjectSrcDirectory / "OLCTemplate.h";
+			std::filesystem::path projectProjectSrcOLCTemplateCPP = projectProjectSrcDirectory / "OLCTemplate.cpp";
+			rc = ReplaceOLCTemplateWithProjectNameInFiles(
+			{
+				projectDirectory / "Dependencies/Dependencies.lua",
+				projectProjectSrcOLCTemplateH,
+				projectProjectSrcOLCTemplateCPP,
+				projectProjectSrcDirectory / "main.cpp"
+			}, argv[1]);
+			if (rc != ReturnCode::Success)
+				return rc;
+
+			std::filesystem::path destName = projectProjectSrcDirectory / argv[1];
+			std::filesystem::rename(projectProjectSrcOLCTemplateH, destName.replace_extension("h"), error);
+			std::filesystem::rename(projectProjectSrcOLCTemplateCPP, destName.replace_extension("cpp"), error);
+		}
+
+		// Call "GenerateProjects.bat" in the appropriate directory.
+		command = "pushd \"";
+		command.append(projectDirectory.string());
+		command.append(1, std::filesystem::path::preferred_separator);
+		command.append("Scripts");
+		command.append(1, std::filesystem::path::preferred_separator);
+		command.append("\" && GenerateProjects.bat && popd");
+		if (system(command.c_str()))
+			return ReturnCode::CouldntGenerateProjects;
+
+		// Open visual studio solution.
+		command = START_PROCESS_BEGIN "\"\" \"";
+		command.append(projectDirectory.string());
+		command.append(1, std::filesystem::path::preferred_separator);
+		command.append(argv[1]);
+		command.append(".sln\"" START_PROCESS_END);
+		if (system(command.c_str()))
+			return ReturnCode::CouldntOpenVSSolution;
+	}
 	else
-		showHelpMessage = true;
+		return ReturnCode::ShowHelpMessage;
 
-	if (showHelpMessage)
-		std::cout << "Usage: " << argv[0] << " <ProjectName> [dir]\n";
+	return ReturnCode::Success;
+}
 
-	return 0;
+ReturnCode EditFile(const std::filesystem::path& filepath, const std::function<void(std::string&)>& func)
+{
+	std::ifstream inFile(filepath);
+	if (!inFile.is_open())
+		return ReturnCode::CouldntReadFile;
+
+	std::stringstream inFileStream;
+	inFileStream << inFile.rdbuf();
+	inFile.close();
+
+	std::string file = inFileStream.str();
+	func(file);
+
+	std::ofstream outFile(filepath);
+	if (!outFile.is_open())
+		return ReturnCode::CouldntWriteFile;
+
+	outFile.write(file.c_str(), file.size());
+	outFile.close();
+
+	return ReturnCode::Success;
+}
+
+void ReplaceOLCTemplateWithProjectName(std::string& file, const char* arg1)
+{
+	size_t projectNameLength = strlen(arg1);
+	size_t offset = 0;
+	while (true) // This is completely fine, stop complaining.
+	{
+		offset = file.find(olcName, offset);
+		if (offset == std::string::npos)
+			break;
+		file.replace(offset, olcNameLength, arg1);
+		offset += projectNameLength;
+	}
+}
+
+ReturnCode ReplaceOLCTemplateWithProjectNameInFiles(const std::vector<std::filesystem::path>& filepaths, const char* arg1)
+{
+	ReturnCode rc = ReturnCode::Success;
+	for (size_t i = 0; i < filepaths.size() && rc == ReturnCode::Success; i++)
+	{
+		rc = EditFile(filepaths[i],
+		[arg1](std::string& file)
+		{
+			ReplaceOLCTemplateWithProjectName(file, arg1);
+		});
+	}
+	return rc;
 }
